e26ea6c6012cef8a31a70a58a8cdf87d1c8c05de
xuwen <2809988030@qq.com>
2020-06-03 22:58:26 +0800

    11

--- jianshu/src/store/reducer.js
+++ jianshu/src/store/reducer.js
@@ -1,9 +1,9 @@
-import {combineReducers} from 'redux'
+import { combineReducers } from 'redux'
 // import HeadReducer from '../common/Header/store/reducer'
-import {reducer as HeadReducer} from '../common/Header/store'
+import { reducer as HeadReducer } from '../common/Header/store'
 
-const reducers= combineReducers({
-    header:HeadReducer
+const reducers = combineReducers({
+    header: HeadReducer
 })
 // 导入得是常量
 export default reducers;
