d009a0696f3c4993e56c7e031065c991d1ceaf70
xuwen <xuwen@food-chain.com>
2020-06-11 18:54:15 +0800

    reducer.js

--- jianshu/src/common/Header/store/reducer.js
+++ jianshu/src/common/Header/store/reducer.js
@@ -1,26 +1,37 @@
-const defaultState={
-    focused:false,
-    list:[]
+import actionCreators from './actionCreators'
+
+const defaultState = {
+    focused: false,
+    list: []
 }
 
-export default(state=defaultState,action)=>{
-   
-    if(action.type==='change_input'){
+export default (state = defaultState, action) => {
+    // switch (action.type) {
+    //     case 'change_input':
+    //         focused = true
+    //         break;
+    //     case 'search_input':
+    //         focused = false
+    //         break;
+    //     // return {focused:true}
+    // }
+
+    if (action.type === 'change_input') {
         // const focused=JSON.parse(JSON.stringify(state))
-        return{
-            focused:true
-           
+        return {
+            focused: true
+
         }
-      
+
     }
-    if(action.type==='search_input'){
+    if (action.type === 'search_input') {
         // const  focused=JSON.parse(JSON.stringify(state))
         return {
-            focused:false
+            focused: false
         }
-    
+
     }
     return state
-   
+
 
 }
