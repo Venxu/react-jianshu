7405e19d571ddb06043f72dfb755a9c9f8837b9f
xuwen <xuwen@food-chain.com>
2020-06-12 18:53:43 +0800

    是是是

--- jianshu/src/store/reducer.js
+++ jianshu/src/store/reducer.js
@@ -1,9 +1,12 @@
 import {combineReducers} from 'redux'
 // import HeadReducer from '../common/Header/store/reducer'
+// 将nav和首页的reducer统一放到大的store管理
 import {reducer as HeadReducer} from '../common/Header/store'
+import {reducer as HomeReducer} from '../views/home/store/index'
 
 const reducers= combineReducers({
-    header:HeadReducer
+    header:HeadReducer,
+    Home:HomeReducer
 })
 // 导入得是常量
 export default reducers;
